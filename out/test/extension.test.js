4bb74c21f2af79a1b5497ca377cf187520a36f9d
Matthew Ferreira <Matthew Ferreira>
2017-01-15 21:08:22 -0500

    Added paramcheck tests

--- out/test/extension.test.js
+++ out/test/extension.test.js
@@ -2,15 +2,13 @@
  * EXTENSION.TEST.JS
  * -----------------
  */
-
-import * as assert from "assert";
-import * as vscode from "vscode";
-
-suite("Extension Tests", function() {
-
+"use strict";
+const assert = require("assert");
+suite("Extension Tests", function () {
     // Defines a Mocha unit test
-    test("Something 1", function() {
+    test("Something 1", function () {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
 });
+//# sourceMappingURL=extension.test.js.map
---
+++ test/paramcheck.test.ts
@@ -0,0 +1,42 @@
+/*
+ * PARAMCHECK.TEST.JS
+ * ------------------
+ * Tests functions in 'paramcheck.ts'.
+ */
+
+import * as assert from "assert";
+import * as pc from "../src/paramcheck";
+
+suite("paramcheck tests", paramcheckTests);
+
+function paramcheckTests() {
+    test("isValidStandard: bad standard", testIsValidStandard_BadStandard);
+    test("isValidStandard: allowed standards", testIsValidStandard_AllowedStandards);
+
+    test("isValidPlatform: bad platform", testIsValidPlatform_BadPlatform);
+    test("isValidPlatform: allowed platforms", testIsValidPlatform_AllowedPlatforms);
+}
+
+function testIsValidStandard_BadStandard() {
+    assert.strictEqual(pc.isValidStandard("Pascal"), false);
+}
+
+function testIsValidStandard_AllowedStandards() {
+    const allowedStandards = [ "posix", "c89", "c99", "c11", "c++03", "c++11" ];
+    for (let index = 0; index < allowedStandards.length; index++) {
+        let element = allowedStandards[index];
+        assert.strictEqual(pc.isValidStandard(element), true);
+    }
+}
+
+function testIsValidPlatform_BadPlatform() {
+    assert.strictEqual(pc.isValidPlatform("ENIAC"), false);
+}
+
+function testIsValidPlatform_AllowedPlatforms() {
+    const allowedPlatforms = [ "unix32", "unix64", "win32A", "win32W", "win64", "native" ];
+    for (let index = 0; index < allowedPlatforms.length; index++) {
+        let element = allowedPlatforms[index];
+        assert.strictEqual(pc.isValidPlatform(element), true);
+    }
+}
