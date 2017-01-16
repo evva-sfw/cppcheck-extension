08ea9f99d54179230f1318eaae2e1212f2466d0b
Matthew Ferreira <Matthew Ferreira>
2017-01-15 20:42:24 -0500

    Started test updates

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,19 +1,11 @@
-/* global suite, test */
+/*
+ * EXTENSION.TEST.JS
+ * -----------------
+ */
 
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
+import * as assert from "assert";
+import * as vscode from "vscode";
 
-// The module 'assert' provides assertion methods from node
-var assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-var vscode = require('vscode');
-var myExtension = require('../extension');
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
 
     // Defines a Mocha unit test
