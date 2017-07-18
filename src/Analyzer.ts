bc496b29cbd9a38cec7fac49a13ddfc8c7cc3086
Matthew Ferreira <matt.ferreira@gmail.com>
2017-07-17 22:13:07 -0400

    case change

---
+++ src/Analyzer.ts
@@ -0,0 +1,31 @@
+/**
+ * @author Matthew Ferreira
+ * @file Defines an object that analyzes source code files.
+ */
+
+export interface Analyzer {
+    /**
+     * Runs the analyzer for the linter. The results of analysis are an XML string, for the ease of parsing.
+     * @param config The extension configuration object.
+     * @param workspaceDir The workspace directory.
+     * @return A string containing the result of the analysis, in XML format.
+     */
+    runLintMode(config: {[key:string]:any}, workspaceDir: string): string;
+
+    /**
+     * Runs the analyzer on a single file.
+     * @param config The extension configuration object.
+     * @param fileName The file to analyzer.
+     * @param workspaceDir The workspace directory. This need not contain the file, only the root.
+     * @return A string containing the results of the analysis.
+     */
+    runOnFile(config: {[key:string]:any}, fileName: string, workspaceDir: string): string;
+
+    /**
+     * Runs the analyzer on an entire workspace.
+     * @param config The extension configuration object.
+     * @param workspaceDir The workspace directory.
+     * @return A string containing the results of the analysis.
+     */
+    runOnWorkspace(config: {[key:string]:any}, workspaceDir: string): string;
+}
---
+++ src/Linter.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Matthew Ferreira
+ * @file Defines an object that lints source code files.
+ */
+
+import { DiagnosticCollection } from 'vscode';
+
+export interface Linter {
+    /**
+     * Executes the linter.
+     * @param diagnosticCollection The collection to which lint diagnostics should be written.
+     * @param config The extension configuration object.
+     * @param workspaceDir The workspace directory.
+     */
+    Execute(diagnosticCollection: DiagnosticCollection, config: {[key:string]:any}, workspaceDir: string): void;
+}
