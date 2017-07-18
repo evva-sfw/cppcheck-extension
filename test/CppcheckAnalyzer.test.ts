fe7354e9320a9691952d26002dd9dfe49809cd09
Matthew Ferreira <matt.ferreira@gmail.com>
2017-07-17 22:33:24 -0400

    analyzer tests

--- test/CppcheckAnalyzer.test.ts
+++ test/CppcheckAnalyzer.test.ts
@@ -16,10 +16,53 @@ suite('CppcheckAnalyzer', function() {
             let outputChannel = new MockOutputChannel();
             let userOutput = new MockUserOutput();
             let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
-            let config: {[key:string]:any} = {};
-            config['enable'] = false;
+            let config = { enable: false };
             analyzer.runOnFile(config, 'filename.cpp', 'C:/Workspaces');
             assert.strictEqual(userOutput.wasShowInfo, true);
+        });
+        test('returns "undefined" if not enabled', function() {
+            let outputChannel = new MockOutputChannel();
+            let userOutput = new MockUserOutput();
+            let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
+            let config = { enable: false };
+            let result = analyzer.runOnFile(config, 'filename.cpp', 'C:/Workspace');
+            assert.deepEqual(result, undefined);
+        });
+    });
+    suite('#runOnWorkspace', function() {
+        test('shows a message if not enabled', function() {
+            let outputChannel = new MockOutputChannel();
+            let userOutput = new MockUserOutput();
+            let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
+            let config = { enable: false };
+            analyzer.runOnWorkspace(config, 'C:/Workspace');
+            assert.strictEqual(userOutput.wasShowInfo, true);
+        });
+        test('returns "undefined" if not enabled', function() {
+            let outputChannel = new MockOutputChannel();
+            let userOutput = new MockUserOutput();
+            let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
+            let config = { enable: false };
+            let result = analyzer.runOnWorkspace(config, 'C:/Workspace');
+            assert.deepEqual(result, undefined);
+        });
+    });
+    suite('#runLintMode', function() {
+        test('shows a message if not enabled', function() {
+            let outputChannel = new MockOutputChannel();
+            let userOutput = new MockUserOutput();
+            let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
+            let config = { enable: false };
+            analyzer.runLintMode(config, 'C:/Workspace');
+            assert.strictEqual(userOutput.wasShowInfo, true);
+        });
+        test('returns "undefined" if not enabled', function() {
+            let outputChannel = new MockOutputChannel();
+            let userOutput = new MockUserOutput();
+            let analyzer = new CppcheckAnalyzer(userOutput, outputChannel);
+            let config = { enable: false };
+            let result = analyzer.runLintMode(config, 'C:/Workspace');
+            assert.deepEqual(result, undefined);
         });
     });
 });
